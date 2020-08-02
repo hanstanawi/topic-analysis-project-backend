b84efee5f4c2d41f54450d1a4d878c5a1c7233db
Hans Tanawi <hans.tanawi@outlook.com>
2020-08-02 22:30:58 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'topic_analysis'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Init MySQL
+mysql = MySQL(app)
+CORS(app)
+
+# Get all the articles
+@app.route('/api/articles', methods=['GET'])
+def getAllArticles():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM topic_analysis.articles')
+    result = cur.fetchall()
+    return jsonify(result)
+
+# Get single article
+@app.route('/api/article', methods=['GET'])
+def getArticleById():
+    articleId = request.args.get('articleId')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM topic_analysis.articles WHERE id = %s', articleId)
+    result = cur.fetchone()
+    print(result)
+    return jsonify(result)
+
+# Search for correlated articles
+@app.route('/api/search', methods=['GET'])
+def searchTopics():
+    keyword = request.args.get('keyword', type=str)
+    cur = mysql.connection.cursor()
+    cur.execute(
+        'SELECT * FROM topic_analysis.articles WHERE title = %s', keyword)
+    result = cur.fetchall()
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
