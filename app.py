02ae299df3f4f6ca8a8090579cf2b90e0e8b9415
Hans Tanawi <hans.tanawi@outlook.com>
2020-08-08 14:08:35 +0800

    Added a new db

--- app.py
+++ app.py
@@ -1,34 +1,41 @@
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+from elasticsearch import Elasticsearch
+
 
 app = Flask(__name__)
 
+# Config Elasticsearch
+es = Elasticsearch('http://140.128.102.112:9200')
+
 # Config MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'topic_analysis'
+app.config['MYSQL_HOST'] = '140.128.102.106'
+app.config['MYSQL_USER'] = 'moris'
+app.config['MYSQL_PASSWORD'] = 'moris123'
+app.config['MYSQL_DB'] = 'ptt_data'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Init MySQL
 mysql = MySQL(app)
-CORS(app)
+CORS(app, support_credentials=True)
 
-# Get all the articles
+# Get latest articles
 @app.route('/api/articles', methods=['GET'])
 def getAllArticles():
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM topic_analysis.articles')
+    cur.execute('SELECT * FROM ptt_data.ptt_content ORDER BY tp DESC LIMIT 5')
     result = cur.fetchall()
     return jsonify(result)
 
 # Get single article
 @app.route('/api/article', methods=['GET'])
+@cross_origin()
 def getArticleById():
     articleId = request.args.get('articleId')
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM topic_analysis.articles WHERE id = %s', articleId)
+    cur.execute(
+        'SELECT * FROM ptt_data.ptt_content WHERE url = %s', [articleId])
     result = cur.fetchone()
     return jsonify(result)
 
@@ -38,11 +45,14 @@ def searchTopics():
     keyword = request.args.get('keyword')
     cur = mysql.connection.cursor()
     cur.execute(
-        'SELECT * FROM topic_analysis.articles WHERE content LIKE %s', ("%{}%".format(
+        'SELECT * FROM ptt_data.ptt_content WHERE content LIKE %s LIMIT 10', ("%{}%".format(
             keyword),))
     result = cur.fetchall()
     return jsonify(result)
 
+
+# Elasticsearch search index
+print(es.search(index='ptt'))
 
 if __name__ == '__main__':
     app.run(debug=True)
